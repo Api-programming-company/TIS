f2dc59049c171d793ea8dc429a915c603d9f4abb
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-10-08 14:18:09 -0400

    cambiar logica de seeders

--- backend/database/seeders/TeacherAndAcademicPeriod.php
+++ backend/database/seeders/TeacherAndAcademicPeriod.php
@@ -9,59 +9,68 @@ use Carbon\Carbon;
 
 class TeacherAndAcademicPeriod extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    public static $periodo2024Id;
+    public static $periodo2021_1Id;
+    public static $periodo2021_2Id;
+
+    public static $borisId;
+    public static $porDesignarId;
+
     public function run()
     {
-
-        User::create([
-            'id' => 1,
+        // Crear usuarios
+        $boris = User::create([
             'first_name' => 'BORIS MARCELO',
             'last_name' => 'CALANCHA NAVIA',
             'email' => 'boris@fcyt.umss.edu.bo',
             'password' => bcrypt('Boris1234*'),
             'user_type' => 'D',
-            'email_verified_at' => Carbon::now(), 
-            'created_at' => Carbon::now(), 
-            'updated_at' => Carbon::now(), 
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
-        User::create([
-            'id' => 2,
+
+        $porDesignar = User::create([
             'first_name' => 'Por',
             'last_name' => 'Designar',
             'email' => 'pordesignar@fcyt.umss.edu.bo',
             'password' => bcrypt('Docente1234*'),
             'user_type' => 'D',
-            'email_verified_at' => Carbon::now(), 
-            'created_at' => Carbon::now(), 
-            'updated_at' => Carbon::now(), 
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
-        AcademicPeriod::create([
-            'id' => 1,
+
+        self::$borisId = $boris->id;
+        self::$porDesignarId = $porDesignar->id;
+
+        // Crear periodos académicos
+        $periodo2024 = AcademicPeriod::create([
             'name' => '2do semestre 2024',
             'start_date' => '2024-06-01',
             'end_date' => '2024-12-15',
             'description' => '2do semestre 2024 de tis docente Boris Calancha',
-            'user_id' => 1,
+            'user_id' => $boris->id,
         ]);
-        AcademicPeriod::create([
-            'id' => 2,
+
+        $periodo2021_1 = AcademicPeriod::create([
             'name' => '1er semestre 2021',
             'start_date' => '2021-01-01',
             'end_date' => '2021-06-15',
             'description' => '1er semestre 2021 de tis docente Boris Calancha',
-            'user_id' => 1,
+            'user_id' => $boris->id,
         ]);
-        AcademicPeriod::create([
-            'id' => 3,
+
+        $periodo2021_2 = AcademicPeriod::create([
             'name' => '2do semestre 2021',
             'start_date' => '2021-06-01',
             'end_date' => '2021-12-15',
             'description' => '2do semestre 2021 de tis docente Por Designar',
-            'user_id' => 2,
+            'user_id' => $porDesignar->id,
         ]);
+
+        self::$periodo2024Id = $periodo2024->id;
+        self::$periodo2021_1Id = $periodo2021_1->id;
+        self::$periodo2021_2Id = $periodo2021_2->id;
     }
 }
